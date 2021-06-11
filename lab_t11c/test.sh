cdbc96dea3e4138423408db0f570d1d17e8de0d7
Bugdi2 <81387504+Bugdi2@users.noreply.github.com>
2021-06-11 17:29:53 +0300

    первая

---
+++ lab_t11c/test.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+echo $HOME
+echo "==========="
+echo $USER
+echo "=========="
+who
+echo "=========="
+ls ~
+echo "==========="
+cat test3 | head -n 3 | tail -n 1
+echo "==========="
+cat "hello world"/main.cpp
+echo "==========="
+./"hello world"/main.out
+echo "============"
+ls ../..
+echo "============"
+cal 06 1999 > birth
+
