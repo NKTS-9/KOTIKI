bc503472a049acc0222484e5b25a8cdaed999cbd
Bugdi2 <81387504+Bugdi2@users.noreply.github.com>
2021-06-14 11:44:28 +0300

    Create 30.sh

---
+++ т23/30.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "#include <iostream>" >./main.cpp
+echo "using namespace std;" >>./main.cpp
+echo "int main ()" >>./main.cpp
+echo "{" >>./main.cpp
+echo "cout <<\"Hello,Bogdan!\"<< endl;" >>./main.cpp
+echo "return 0;" >>./main.cpp
+echo "}" >>./main.cpp
+
+
+g++ main.cpp -o main.out;
